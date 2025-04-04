a1eb77d4b84d73017d08ed61b0e6f532c9ebf52a
springveil27 <springveil27@gmail.com>
2025-04-04 18:37:26 -0400

    Initial commit

---
+++ entities model/Reserva.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace turipaq.entities_model
+{
+    internal class Reserva
+    {
+    }
+}
